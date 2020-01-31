c2f01d96be1d57f9ab84e38967c47cfe067b7ddb
ajavieralejandro <amorosijavier@gmail.com>
2020-01-31 03:28:32 -0300

    toggle cart hidden with mutation on cart dropdown

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,8 +1,13 @@
 import {gql} from 'apollo-boost';
+import {AddItemToCart, addItemToCart} from './cart.utils';
 
 export const typeDefs = gql`
+    extend type Item{
+        quantity : Int
+    }
     extend type Mutation{
-        ToggleCartHidden : Boolean!
+        ToggleCartHidden : Boolean!,
+        AddItemToCart(item : Item!): [Item]!
 
     } 
 `
@@ -11,8 +16,14 @@ const GET_CART_HIDDEN = gql`{
     cartHidden @client
 
 }
+`;
 
-`
+const GET_CART_ITEMS = gql`{
+    cartItems @client
+
+}
+
+`;
 
 export const resolvers = {
     Mutation : {
@@ -23,6 +34,16 @@ export const resolvers = {
             data : {cartHidden: !cartHidden}
         })
         return cartHidden;
+        },
+
+        addItemToCart : (_root,{item},{cache})=>{
+            const {cartItems} = cache.readQuery({query:GET_CART_ITEMS});
+            const newCartItems = addItemToCart(cartItems,item);
+            cache.writeQuery({
+                query : GET_CART_ITEMS,
+                data : {cartItems : newCartItems}
+            })
+
         }
 
 
