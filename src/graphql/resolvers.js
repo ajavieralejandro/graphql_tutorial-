421cc14e28a3783faabca1255d5913232ef365fb
ajavieralejandro <amorosijavier@gmail.com>
2020-02-01 00:51:11 -0300

    cartCount query

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,5 @@
 import {gql} from 'apollo-boost';
-import {addItemToCart} from './cart.utils';
+import {addItemToCart,getCartItemCount} from './cart.utils';
 
 export const typeDefs = gql`
     extend type Item{
@@ -15,6 +15,11 @@ export const typeDefs = gql`
 const GET_CART_HIDDEN = gql`{
     cartHidden @client
 
+}
+`;
+
+const GET_ITEM_COUNT = gql`{
+    itemCount @client
 }
 `;
 
@@ -39,6 +44,12 @@ export const resolvers = {
         addItemToCart : (_root,{item},{cache})=>{
             const {cartItems} = cache.readQuery({query:GET_CART_ITEMS});
             const newCartItems = addItemToCart(cartItems,item);
+
+            cache.writeQuery({
+                query: GET_ITEM_COUNT,
+                data : {itemCount : getCartItemCount(newCartItems)}
+            })
+
             cache.writeQuery({
                 query : GET_CART_ITEMS,
                 data : {cartItems : newCartItems}
